c6a59b1cd03334e22512474fbd1502e2ddb9f804
AmenNkala <amen@bbd.co.za>
2022-05-06 14:43:46 +0200

    saving changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,34 @@
-const add = (...args) => {
-  args.map(Number);
-  return args.reduce((acc, curr) => acc + curr, 0);
-};
+const answerField = document.getElementById("answer");
 
-const validateInputs = (input) => {};
+const digitsBtns = document.getElementsByClassName("digits");
+
+for (let i = 0; i < digitsBtns.length; i++) {
+  digitsBtns[i].addEventListener("click", () => {
+    answerField.value += digitsBtns[i].value;
+    if (ans !== "") {
+      answerField.value = digitsBtns[i].value;
+    }
+  });
+}
+
+const operatorBtnsArr = Array.from(
+  document.getElementsByClassName("operatorBtn")
+);
+
+operatorBtnsArr.map((button) => {
+  button.addEventListener("click", () => {
+    answerField.value += button.value;
+  });
+});
+let ans = "";
+const eqBtn = document.getElementById("eqBtn");
+eqBtn.addEventListener("click", () => {
+  let str = answerField.value;
+
+  if (/[=]/g.test(str) || str === "") {
+    answerField.value = "";
+  } else {
+    ans = `${Function(`return ${answerField.value};`)()}`;
+    answerField.value = ans;
+  }
+});
