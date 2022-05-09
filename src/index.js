e83d1761275ce6c407542f022c2db85481408e58
AmenNkala <amennkala@gmail.com>
2022-05-09 14:57:47 +0200

    final changes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,29 @@
-const answerField = document.getElementById("answer");
+//import { basicCompute } from "./basicCalc";
+
+const modifyArray = (array1, array2, index, operator) => {
+  let temp_ans = Function(
+    `return ${array1[index]} ${operator} ${array1[index + 1]}`
+  )();
+  array1.splice(index, 2, temp_ans);
+  array2.splice(index, 1);
+};
+
+const basicCompute = (expression) => {
+  const operands = expression.match(/-?[\d{1,}\.]{1,}/g).map(Number);
+  const operators = expression.replaceAll(/[0-9\.]/gi, "").split("");
+  for (let i = 0; i < operators.length; i++) {
+    if (operators[i] == "*" || operators[i] == "/") {
+      modifyArray(operands, operators, i, operators[i]);
+      i = -1;
+    }
+  }
+  for (let i = 0; i < operators.length; i++) {
+    modifyArray(operands, operators, i, "+");
+    i = -1;
+  }
+  return operands[0];
+};
+const output = document.getElementById("output");
 
 const digitsBtns = document.getElementsByClassName("digits");
 
